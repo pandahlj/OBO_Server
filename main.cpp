24e9271ef914d81c5f566fe0d9a8b8fd8f42cd41
xc <luffy@192.168.1.4>
2022-12-09 21:07:37 +0800

    add test func

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+void test()
+{
+    std::cout << "It's a test func." << std::endl;
+}
+
 int main()
 {
     std::cout << "It is a test for git." << std::endl;
     std::cout << "Dose man should love a woman?" << std::endl;
+
+    test();
     
     return 0;
 }
