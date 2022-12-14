ae3b4547c2c9f6e534ca0033addec75b3390367f
xc <luffy@192.168.1.6>
2022-12-14 20:11:48 +0800

    add while in main func

--- main.cpp
+++ main.cpp
@@ -10,7 +10,13 @@ int main()
     std::cout << "It is a test for git." << std::endl;
     std::cout << "Dose man should love a woman?" << std::endl;
 
-    test();
+    threadTest();
+
+    while(true)
+    {
+        std::cout << "main thread." << std::endl;
+        sleep(1);
+    }
     
     return 0;
 }
